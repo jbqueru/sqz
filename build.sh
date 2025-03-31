5f1e3fc2415f49b0f27860b5597afcc96ef24b64
Jean-Baptiste Queru <jbqueru@gmail.com>
2025-03-31 21:12:00 +0300

    Remove gcc test

--- build.sh
+++ build.sh
@@ -32,9 +32,6 @@ out/bin/test_bitstream || exit $?
 echo '(*) build Squeezer tool'
 gcc sqz.c bitstream.c cmdline.c debug.c degas.c exitcodes.c huffman.c -O2 -Wall -Wextra -o out/bin/sqz || exit $?
 
-echo '(*) build gcc investigation tool'
-gcc sqz0.c -O1 -Wall -o out/tmp/sqz0 || exit $?
-
 #echo '(*) run Squeezer tool'
 #out/bin/sqz out/tos/VMAX.PI1 || exit $?
 
