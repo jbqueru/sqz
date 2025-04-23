9f9aa559a91bf3beb0e38f5846ecf2cf1104a8a6
Jean-Baptiste Queru <jbqueru@gmail.com>
2025-04-23 11:01:19 +0300

    Prepare for compression options

--- cmdline.h
+++ cmdline.h
@@ -20,8 +20,54 @@
 #ifndef CMDLINE_H_INCLUDED
 #define CMDLINE_H_INCLUDED
 
+enum delta {
+	DELTA_UNSPECIFIED = 0,
+    DELTA_ANY,
+    DELTA_NONE,
+    DELTA_ARITHMETIC_1D,
+    DELTA_ARITHMETIC_2D,
+    DELTA_WRAP_1D,
+    DELTA_WRAP_2D,
+    DELTA_XOR_1D,
+    DELTA_XOR_2D,
+};
+
+enum pixel_order {
+	ORDER_UNSPECIFIED = 0,
+	ORDER_ANY,
+	ORDER_HORIZONTAL_PIXEL,
+	ORDER_VERTICAL_PIXEL,
+	ORDER_HILBERT_PIXEL,
+	ORDER_HORIZONTAL_CHARACTER,
+	ORDER_VERTICAL_CHARACTER,
+	ORDER_HILBERT_CHARACTER,
+};
+
+enum transform {
+	TRANSFORM_UNSPECIFIED = 0,
+	TRANSFORM_ANY,
+	TRANSFORM_NONE,
+	TRANSFORM_BURROWS_WHEELER,
+	TRANSFORM_MOVE_TO_FRONT,
+	TRANSFORM_DELTA_ARITHMETIC,
+	TRANSFORM_DELTA_WRAP,
+	TRANSFORM_DELTA_XOR,
+};
+
+enum compression {
+	COMPRESSION_UNSPECIFIED = 0,
+	COMPRESSION_ANY,
+	COMPRESSION_NONE,
+	COMPRESSION_LZ78,
+	COMPRESSION_HUFFMAN,
+};
+
 extern char* cmdline_inputfilename;
 extern char* cmdline_outputfilename;
+extern enum delta cmdline_delta;
+extern enum pixel_order cmdline_pixelorder;
+extern enum transform cmdline_transform[10];
+extern enum compression cmdline_compression[10];
 
 void parse_cmdline(int argc, char** argv);
 void display_version();
